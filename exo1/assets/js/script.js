35305f38e9a85297faa19bd8419c9f3804ba413a
StephaneC <stephane.cavillon@itech.fr>
2020-11-10 15:04:03 +0100

    fin de l'exo1

---
+++ exo1/assets/js/script.js
@@ -0,0 +1,18 @@
+// Création du lien pour la div
+let div = document.querySelector('div');
+
+// Création de la boucles pour l'affichage des années
+for(let i = 2020; i <= 2030; i++) {
+        
+    // Création de la règle pour les années bisextiles
+    if(i % 4 == 0 && i % 100 !== 0 || i % 400 == 0){
+        // on reprend le contenu precedent et on vient lui ajouter une div de couleur avec la nouvelle valeur
+        div.innerHTML = div.innerHTML + '<div style="color: red">' + i + '</div>';
+    } else {
+        div.innerHTML = div.innerHTML + '<div style="color: black">' + i + '</div>';
+    };
+
+};
+
+
+
